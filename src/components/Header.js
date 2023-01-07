a8430df80fea167fb86ccbe9a4f9f509a471b6aa
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 22:08:45 +0200

    update: bottomNavbar done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import {SearchIcon, ShoppingCartIcon} from '@heroicons/react/outline';
+import {MenuIcon, SearchIcon, ShoppingCartIcon} from '@heroicons/react/outline';
 
 export default function Header() {
   return (
@@ -39,8 +39,20 @@ export default function Header() {
             </div>
         </div>
         {/* Bottom Nav */}
-        <div>
-            <h2>TESTING</h2>
+        <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
+            <p className='link flex items-center'>
+                <MenuIcon className='h-6 mr-1' />
+                All
+            </p>
+            <p className='link'>Prime Video</p>
+            <p className='link'>Amazon Business</p>
+            <p className='link'>Today's Deals</p>
+            <p className='link hidden lg:inline-flex'>Eletronics</p>
+            <p className='link hidden lg:inline-flex'>Food & Grocery</p>
+            <p className='link hidden lg:inline-flex'>Prime</p>
+            <p className='link hidden lg:inline-flex'>Buy Again</p>
+            <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
+            <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
         </div>
     </header>
   )
