5cdb0efb1a1d7ae84c0bde1dfaa28e6a1ae201c5
Ntshangase <simphiwe323i@gmail.com>
2023-01-08 01:28:53 +0200

    update: batterlow

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -4,18 +4,19 @@ import Product from './Product';
 export default function ProductFeed({ products }) {
     return (
         <div>
-            <h1>ProductFeed...</h1>
-            {products.map(({ id, tittle, price, description, category, image }) => (
-                <Product
-                    key={id}
-                    id={id}
-                    tittle={tittle}
-                    price={price}
-                    description={description}
-                    category={category}
-                    image={image}
-                />
-            ))}
+            <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+                {products.map(({ id, tittle, price, description, category, image }) => (
+                    <Product
+                        key={id}
+                        id={id}
+                        tittle={tittle}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
