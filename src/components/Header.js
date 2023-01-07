3ad6734cb1d5604f2da8925220e76fbeae073fd8
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 12:42:40 +0200

    update: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header>I'm a Header</header>
+    <header>
+        {/* Top Nav*/}
+        <div>
+            <div>
+                <Image
+                src="https://links.papareact.com/f90"
+                width={150}
+                height={40} 
+                objectFit="contain"
+                className="cursor-pointer"
+                />
+            </div>
+        </div>
+        {/* Bottom Nav */}
+        <div>
+            <h2>TESTING</h2>
+        </div>
+    </header>
   )
 }
