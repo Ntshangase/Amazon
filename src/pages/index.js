18493a6c04f4dde6db3e139b62a9d12e13dbbfa3
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 10:20:59 +0200

    feat: Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      <h1>Zulu</h1>
+      <Header />
     </div>
   );
 }
