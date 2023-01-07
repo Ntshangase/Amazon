88e9c6acfd51677f6b6a56ef873dbfb98b4396df
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 12:52:15 +0200

    feat: header seach

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header>
         {/* Top Nav*/}
-        <div>
-            <div>
+        <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+            <div className='mt-2 flex items-center flex-grow sm:flex-grow-0' >
                 <Image
                 src="https://links.papareact.com/f90"
                 width={150}
@@ -14,6 +14,9 @@ export default function Header() {
                 objectFit="contain"
                 className="cursor-pointer"
                 />
+            </div>
+            <div>
+                <input type="text" />
             </div>
         </div>
         {/* Bottom Nav */}
