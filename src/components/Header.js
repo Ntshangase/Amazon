b66a28507d92a832e17ca8847bcbb35d71340d42
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 21:02:22 +0200

    update: header input done

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,8 +16,8 @@ export default function Header() {
                 className="cursor-pointer"
                 />
             </div>
-            <div>
-                <input type="text" />
+            <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hoover-bg-yellow-500'>
+                <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' type="text" />
                 <SearchIcon className='h-12 p-4' />
             </div>
         </div>
