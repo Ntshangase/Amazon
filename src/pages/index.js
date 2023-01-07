75c7e11310a96ad2171ad395954b4b9c41eec766
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 23:20:37 +0200

    update: banner color

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
