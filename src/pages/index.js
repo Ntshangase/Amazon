dd29f845ba0f285ecfcd5b643c077cd861f43706
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 22:49:16 +0200

    feat: Banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -8,6 +9,10 @@ export default function Home() {
         <title>Amazon 2.0</title>
       </Head>
       <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        {/**Product */}
+      </main>
     </div>
   );
 }
