4f0b8420e220a42a5bdfbce3672730c505a49560
Ntshangase <simphiwe323i@gmail.com>
2023-01-07 21:49:13 +0200

    update: topHeader done

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,17 +23,18 @@ export default function Header() {
             </div>
             {/*Right side */}
             <div className=' text-white flex items-center text-xs space-x-6 whitespace-nowrap'>
-                <div>
+                <div className='link'>
                     <p>Hello Ntshangase</p>
-                    <p>Account & Lists</p>
+                    <p className='font-extrabold md:text-sm'>Account & Lists</p>
                 </div>
-                <div>
+                <div className='link'>
                     <p>Returns</p>
-                    <p>Orders</p>
+                    <p className='font-extrabold md:text-sm'>Orders</p>
                 </div>
-                <div> 
+                <div className='relative link flex items-center'> 
+                    <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold' >0</span>
                     <ShoppingCartIcon className='h-10' />
-                    <p>Basket</p>
+                    <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
                 </div>
             </div>
         </div>
