f2eebd8ba3a9e36b582829b6a04b090b6569b633
Ntshangase <simphiwe323i@gmail.com>
2023-01-09 09:14:18 +0200

    Installed: nextAuth

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxuAfEZcgFjVY2uZkg-lmzwldB1eya9Jo",
+    authDomain: "fir-5d727.firebaseapp.com",
+    projectId: "fir-5d727",
+    storageBucket: "fir-5d727.appspot.com",
+    messagingSenderId: "501941152655",
+    appId: "1:501941152655:web:4f0f2b30a4601c301839fa",
+    measurementId: "G-LBNNTMSV16"
+  };
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,37 +3,24 @@ import Product from './Product';
 
 export default function ProductFeed({ products }) {
     return (
-        
-            <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
-                {products.slice(0,4).map(({ id, tittle, price, description, category, image }) => (
-                    <Product
-                        key={id}
-                        id={id}
-                        tittle={tittle}
-                        price={price}
-                        description={description}
-                        category={category}
-                        image={image}
-                    />
-                ))}
 
-                <img className='md:col-span-full' src='https://links.papareact.com/dyz' alt='' />
-                
-                <div className='md:col-span-2'>
-                {products.slice(4,5).map(({ id, tittle, price, description, category, image }) => (
-                    <Product
-                        key={id}
-                        id={id}
-                        tittle={tittle}
-                        price={price}
-                        description={description}
-                        category={category}
-                        image={image}
-                    />
-                ))}
-                </div>
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+            {products.slice(0, 4).map(({ id, tittle, price, description, category, image }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    tittle={tittle}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            ))}
 
-                {products.slice(5,products.length).map(({ id, tittle, price, description, category, image }) => (
+            <img className='md:col-span-full' src='https://links.papareact.com/dyz' alt='' />
+
+            <div className='md:col-span-2'>
+                {products.slice(4, 5).map(({ id, tittle, price, description, category, image }) => (
                     <Product
                         key={id}
                         id={id}
@@ -44,6 +31,19 @@ export default function ProductFeed({ products }) {
                         image={image}
                     />
                 ))}
-            </div>  
+            </div>
+
+            {products.slice(5, products.length).map(({ id, tittle, price, description, category, image }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    tittle={tittle}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            ))}
+        </div>
     );
 }
