fd68e068befa1e07b83071e3eb311ee7c209d0e2
Ntshangase <simphiwe323i@gmail.com>
2023-01-09 23:29:47 +0200

    update: Checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import Header from '../components/Header'
+import Header from '../components/Header';
+import Image from 'next/image';
 
 export default function checkout() {
-  return (
-    <div className='bg-gray-100'>
-        <Header />
-        <h2>checkout page test</h2>
-    </div>
-  )
+    return (
+        <div className='bg-gray-100'>
+            <Header />
+
+            <main className='lg:flex max-w-screen-2xl mx-auto'>
+                {/**Left */}
+                <div className='flex-grow m-5 shadow-sm'>
+                    <Image src="https://links.papareact.com/ikj"
+                        width={1020}
+                        height={250}
+                        objectFit="contain"
+                    />
+                    <div className='flex flex-col p-5 space-y-10 bg-white'>
+                        <h1 className='text-3xl border-b bp-4'>Your Shopping Basket</h1>
+                    </div>
+                </div>
+
+                {/**Right-side */}
+
+            </main>
+
+        </div>
+    )
 }
